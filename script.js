5e011731cb5485d75a3bdcb82a968ebf276bbac3
Nethmini Suraji Hewage <nethminisuraji@gmail.com>
2024-11-15 17:14:01 +0530

    First Commit - committing current Main UI files to the repository

---
+++ script.js
@@ -0,0 +1,9 @@
+// Redirect to login page
+function redirectToLogin() {
+    window.location.href = 'login.php'; // Replace with your actual login page path
+}
+
+// Redirect to signup page
+function redirectToSignUp() {
+    window.location.href = 'signup.php'; // Replace with your actual signup page path
+}
