f6407b2d3292003351f30eca4452a6009ee6cd8d
Nethmini Suraji Hewage <nethminisuraji@gmail.com>
2024-11-17 02:26:38 +0530

    Modified Script

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Redirect to login page
 function redirectToLogin() {
-    window.location.href = 'login.php'; // Replace with your actual login page path
+    window.location.href = 'login.php';
 }
 
 // Redirect to signup page
 function redirectToSignUp() {
-    window.location.href = 'signup.php'; // Replace with your actual signup page path
+    window.location.href = 'signup.php';
 }
