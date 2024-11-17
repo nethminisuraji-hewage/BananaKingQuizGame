34d63a8990bcb787fed4c927f87f71f6d9b01d76
Nethmini Suraji Hewage <nethminisuraji@gmail.com>
2024-11-17 11:04:43 +0530

    Committing a small change

--- play_game.php
+++ play_game.php
@@ -3,7 +3,7 @@ session_start();
 
 // Redirect to login if not logged in
 if (!isset($_SESSION['username'])) {
-    header("Location: index.php");
+    header("Location: index.html");
     exit();
 }
 
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['level'])) {
     // Re-initialize gameplay variables when the level changes
     $_SESSION['score'] = 0;
     $_SESSION['lives'] = 5; 
-    $_SESSION['round'] = 1; 
+    $_SESSION['round'] = 1;
 }
 
 // Check if session variables are properly set
